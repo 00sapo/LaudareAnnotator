e651955f86b29e9b3c5b58a5379b55090b8f2c1d
Federico Simonetta <federico.simonetta@gssi.it>
2023-06-29 17:13:01 +0200

    Mainly added groups and prepared for callbacks

--- laudare/main.py
+++ laudare/main.py
@@ -7,6 +7,8 @@ import gui
 import inkex
 import utils
 
+from gi.repository import GLib
+
 
 def to_px(value, unit):
     if unit != "px":
@@ -15,7 +17,7 @@ def to_px(value, unit):
         return value
 
 
-def process_node(node):
+def node_to_annotation(node, children=[]):
     if node.tag_name == "text":
         bbox = node.get_inkscape_bbox()  # slow, this is calling inkscape command!
     else:
@@ -26,6 +28,7 @@ def process_node(node):
         "w": to_px(bbox.width, node.unit),
         "h": to_px(bbox.height, node.unit),
         "text": node.get_text() if node.tag_name == "text" else None,
+        "children": [c.get_id() for c in children],
     }
 
 
@@ -46,11 +49,12 @@ class LaudareExtension(inkex.extensions.OutputExtension):
         self.gui.set_palette(utils.get_svg_palette(self.svg))
         self.gui.start()
 
-    def save_annotations(self, button):
+    def save_annotations(self, callback=None, args=None):
         """Export the SVG file itself into the JSON file, using the associations defined
         by the widgets and destroy the window"""
         json_data = {}
         all_elements = self.svg.descendants()
+        all_groups = all_elements.get(inkex.Group)
         image = all_elements.get(inkex.Image)
         if len(image) > 1:
             raise RuntimeError("SVG has multiple images, not supported")
@@ -58,12 +62,13 @@ class LaudareExtension(inkex.extensions.OutputExtension):
             raise RuntimeError("SVG has no image, not supported")
         else:
             image = image[0]
-        json_data["metadata"] = {
+        json_data["info"] = {
             "unit": "px",
             "image": (image.top, image.left, image.width, image.height),
             "date": datetime.datetime.now().isoformat(),
             "author": getpass.getuser(),
         }
+        json_data["annotations"] = {}
         for label, (obj, color, isgroup) in self.gui.get_association_dict().items():
             # get all elements of type obj
             inkex_class = self.object_types[obj]
@@ -77,26 +82,40 @@ class LaudareExtension(inkex.extensions.OutputExtension):
                 if color in [color_fill, color_stroke]:
                     obj_elements_color.append(node)
 
-            json_data[label] = {}
+            json_data["annotations"][label] = {}
 
             if not isgroup:
                 # add the element to the json data
                 with concurrent.futures.ThreadPoolExecutor() as executor:
                     futures = [
-                        executor.submit(process_node, node)
+                        executor.submit(node_to_annotation, node)
                         for node in obj_elements_color
                     ]
                     for i, future in enumerate(
                         concurrent.futures.as_completed(futures)
                     ):
-                        label = obj_elements_color[i].get_id()
-                        json_data[label] = future.result()
+                        id = obj_elements_color[i].get_id()
+                        json_data["annotations"][label][id] = future.result()
             else:
-                # TODO
-                pass
-
+                # iterate all groups and selects only those that contain obj with
+                # color
+                for group in all_groups:
+                    if group.groupmode == "layer":
+                        continue
+                    grouped_nodes = []
+                    for node in group.descendants():
+                        color_fill = utils.get_node_color(node, "fill")
+                        color_stroke = utils.get_node_color(node, "stroke")
+                        if color in [color_fill, color_stroke]:
+                            grouped_nodes.append(node)
+                    if len(grouped_nodes) > 1:
+                        json_data["annotations"][label][
+                            group.get_id()
+                        ] = node_to_annotation(group, children=grouped_nodes)
         pprint(json_data)
-        self.gui.stop()
+        if callback is not None:
+            callback(*args)
+        # self.gui.stop()
 
 
 if __name__ == "__main__":
